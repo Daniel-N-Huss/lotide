74719c2b89ce85b9bb4e8feebac989194cbfa5ba
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-04-28 01:50:26 +0000

    Created a function to test if to parameters are equal, with STYLE

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log('\u2705' + ' ' + actual + ' === ' + expected);
+  } else {
+    console.log('\u274C' + ' ' + actual + ' !=== ' + expected);
+  }
+};
+
+// tests
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, true);
+assertEqual('bones', 'Bones');
+assertEqual('100', 100);
+assertEqual(101, 100);
+assertEqual('Daniel', 'Daniel');
