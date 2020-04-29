a67827ef51890d753719ede5be893abb0fe5d6ca
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-04-29 04:15:42 +0000

    created function that can scub out values in an array, listed in another array

---
+++ without.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length || i < arr2.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`\u2705 ${array1} === ${array2}`);
+  } else {
+    console.log(`\u274C ${array1} !== ${array2}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let sourceToEdit = [...source];
+  for (let x = 0; x < source.length; x++) {
+    for (let y = 0; y < itemsToRemove.length; y++) {
+      if (source[x] === itemsToRemove[y]) {
+        sourceToEdit.splice(x, 1);
+      }
+    }
+  }
+  console.log(source);
+  return sourceToEdit;
+};
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without(['aba', 'bcb', 'b'], ['aba', 'b']));
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]));
+
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
