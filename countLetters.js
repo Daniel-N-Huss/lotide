555b4f0b8564dc04044a1e72ced993576e4867f4
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-04-29 22:18:17 +0000

    initial creation of countLetters function. Currently empty

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`\u2705 ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274C ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function(sentence) {
+  
+};
