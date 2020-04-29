fc4c5c74e40ae008f91ce9fb7fd6ed4c338b7d8a
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-04-29 23:42:09 +0000

    finished letter count from string function, including lint and tests

--- countLetters.js
+++ countLetters.js
@@ -11,14 +11,21 @@ const assertEqual = function(actual, expected) {
 const countLetters = function(sentence) {
   const letterCount = {};
   for (let letter of sentence) {
-    if (letterCount[letter]) {
-      letterCount[letter] += 1;
-    } else {
-      letterCount[letter] = 1;
+    if (letter !== ' ') {
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
     }
   }
   return letterCount;
 };
+
+const test = countLetters('bee');
+
+assertEqual(test['b'], 1);
+assertEqual(test['e'], 2);
 
 
 console.log(countLetters('Hello there'));
