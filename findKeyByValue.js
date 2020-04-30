d302654cb92bd69a9867105f41337e49334b5838
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-04-30 03:19:52 +0000

    Big commit, full function included with tests

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+///tests
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`\u2705 ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274C ${actual} !== ${expected}`);
+  }
+};
+
+
+const birthYears = {
+  Daniel: '1990',
+  Bob: '1980',
+  George: '1667'
+};
+
+assertEqual(findKeyByValue(birthYears, '1667'), "George");
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
