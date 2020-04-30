5e5499ee16ac14252b4a53c44621f48451739700
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-04-30 03:25:37 +0000

    init with assertEqual test included

---
+++ eqObjects.js
@@ -0,0 +1,18 @@
+const eqObjects = function(obj1, obj2) {
+
+};
+
+
+
+
+
+
+// Tests
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`\u2705 ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274C ${actual} !== ${expected}`);
+  }
+};
