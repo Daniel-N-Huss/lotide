c0afc5e54cd2ee5f3f54db2d506b754abdbdd5ba
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-01 02:09:41 +0000

    findKey works, and refactored to accept nested objects or a flat object

--- findKey.js
+++ findKey.js
@@ -7,36 +7,33 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
 const findKey = function(object, callback) {
-  let result;
   for (const key in object) {
-    if (callback(key)) {
-      return object[key];
+    if (callback(object[key])) {
+      return key;
+    } else if (callback(key)) {
+      return key;
     }
   }
-  return result;
 };
 
 
-
-// const findKeyByValue = function(object, value) {
-  // for (const key in object) {
-    // if (object[key] === value) {
-      // return key;
-    // }
-  // }
-  // return undefined;
-// };
-
-
-
-
-findKey({
+assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2), "noma"); // => "noma"
+
+let zergUnits = {
+  baneling: 'bad',
+  zergling: 'bad',
+  queen: 'good',
+  infestor: 'bad'
+};
+
+assertEqual(findKey(zergUnits, x => x === 'good'), 'queen');
+assertEqual(findKey(zergUnits, x => x === 'bad'), 'baneling');
+assertEqual(findKey(zergUnits, x => x === 'brave'), undefined);
