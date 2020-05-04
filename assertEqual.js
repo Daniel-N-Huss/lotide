0c8bd4384374aae2d69b07df4d3ad835310265d3
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-04 14:36:11 +0000

    refactor to dry code, create seperate test files and folder

--- assertEqual.js
+++ assertEqual.js
@@ -7,12 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// tests
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, true);
-assertEqual('bones', 'Bones');
-assertEqual('100', 100);
-assertEqual(101, 100);
-assertEqual('Daniel', 'Daniel');
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,19 +1,5 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`\u2705 ${actual} === ${expected}`);
-  } else {
-    console.log(`\u274C ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(x) {
   return x[0];
 };
 
-
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]),(1));
-assertEqual(head([]), 5);
+module.exports = head;
